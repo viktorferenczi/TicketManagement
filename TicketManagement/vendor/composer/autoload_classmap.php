bf5a9bbaa362aa8d65975fc24981434df1f22eec
Viktor Ferenczi <ferencziviktor@gmail.com>
2020-09-08 11:12:44 +0200

    database setup: sqlite

--- TicketManagement/vendor/composer/autoload_classmap.php
+++ TicketManagement/vendor/composer/autoload_classmap.php
@@ -23,6 +23,7 @@ return array(
     'App\\Providers\\BroadcastServiceProvider' => $baseDir . '/app/Providers/BroadcastServiceProvider.php',
     'App\\Providers\\EventServiceProvider' => $baseDir . '/app/Providers/EventServiceProvider.php',
     'App\\Providers\\RouteServiceProvider' => $baseDir . '/app/Providers/RouteServiceProvider.php',
+    'App\\Ticket' => $baseDir . '/app/Ticket.php',
     'App\\User' => $baseDir . '/app/User.php',
     'ArithmeticError' => $vendorDir . '/symfony/polyfill-php70/Resources/stubs/ArithmeticError.php',
     'Asm89\\Stack\\Cors' => $vendorDir . '/asm89/stack-cors/src/Cors.php',
