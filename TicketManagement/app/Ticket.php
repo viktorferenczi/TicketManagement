c6c44835f4d176ad5bcaec7652b359ae919a0d82
Viktor Ferenczi <ferencziviktor@gmail.com>
2020-09-08 11:27:05 +0200

    make auth, login-register

--- TicketManagement/app/Ticket.php
+++ TicketManagement/app/Ticket.php
@@ -6,8 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description',
+    ];
+
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class); //relation to user
     }
 
 }
