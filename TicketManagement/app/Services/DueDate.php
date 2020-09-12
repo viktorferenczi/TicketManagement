7b34a50588186b005f6093b67858764d41d5fe1c
Viktor Ferenczi <ferencziviktor@gmail.com>
2020-09-12 17:06:02 +0200

    ticket arrives after working ours&before working ours at any day: DONE. ticket arrives in working day: needs to be tested

--- TicketManagement/app/Services/DueDate.php
+++ TicketManagement/app/Services/DueDate.php
@@ -46,7 +46,7 @@ class DueDate implements DateCalculatorInterface
      */
     public function calculate()
     {
-        $date = Clock::at('2020-09-11 06:00');
+        $date = Clock::at('2020-09-06 06:00');
 
         //case: ticket arrives before 09:00-----------------------------------------------------------------------------
         if($this->isBeforeWorkingHour()){
@@ -65,26 +65,39 @@ class DueDate implements DateCalculatorInterface
 
             $finalDate = $this->plusHoursForNextDay($newDatePlusHours);//skip for tomorrow and add the remaining 8 hours
 
-
+            dd($finalDate);
             return $finalDate->toDateTime(); //final due date if ticket arrives any day before 09:00
 
 
             //case: ticket arrives after 17:00--------------------------------------------------------------------------
         } else if($this->isAfterWorkingHour()) {
-            dd("isafter");
-            $date = $this->fromWeekendsToMonday($date); // check if the submit happened on the weekend
 
-            //skip for the next day and set the time to 09:00 AM that day
-            $date->plusDays(1);
-            $year = substr($date->toDateTime(),0,10);
-            $date = Clock::at($year . " " ."09:00" );
+            $dateTime = $date->toDateTime();
+
+            if($this->isSaturday($dateTime) == true || $this->isSunday($dateTime) == true){ //if it is one of the weekdays
+                $newDate = $this->fromWeekendsToMonday($date); // jump to monday
+            } else {
+                $nextDay = $date->plusDays(1);              // if it is friday, the next day is gonna be a weekend then
+                if($this->isSaturday($nextDay->toDateTime())) {
+                    $newDate =  $date->plusDays(3);         // jump to monday
+                } else {
+                    $newDate = $this->skipForNextDay($date); // it was in weekday, skip for the next week day
+                }
+            }
+
+            //set the time to 09:00 AM that day
+            $year = substr($newDate->toDateTime()->format('Y-m-d H:i:s'),0,10);
+            $newDate = Clock::at($year . " " ."09:00" );
+
+            $newDatePlusHours = $newDate->plusHours(8); //added 8 hours for that day, we are now at 17:00 (same day) - remaining hours: 8
+
+            $finalDate = $this->plusHoursForNextDay($newDatePlusHours); //skip for tomorrow and add the remaining 8 hours
+
+            dd($finalDate);
+            return $finalDate->toDateTime(); //final due date if ticket arrives any day after 09:00
 
 
-            $date->plusHours(8); //added 8 hours for that day, we are now at 17:00 (same day) - remaining hours: 8
 
-            $date = $this->plusHoursForNextDay($date); //skip for tomorrow and add the remaining 8 hours
-
-            return $date->toDateTime(); //final due date if ticket arrives any day after 09:00
 
 
             //case: ticket arrives in working hours---------------------------------------------------------------------
@@ -96,6 +109,19 @@ class DueDate implements DateCalculatorInterface
         }
     }
 
+    /***
+     * if not weekend, skip for the next day
+     *
+     * @return Clock
+     */
+    public function skipForNextDay($date){
+        if(($this->isSaturday($date->toDateTime()) == false && $this->isSunday($date->toDateTime()) == false)){
+            $newDate = $date->plusDays(1);
+            return $newDate;
+        }
+            return $date;
+    }
+
 
     /***
      * check if the date is before working hour
@@ -103,7 +129,7 @@ class DueDate implements DateCalculatorInterface
      * @return bool
      */
     public function isBeforeWorkingHour(){
-        $date = Clock::now();
+        $date = Clock::at('2020-09-06 06:00');
         $year = substr($date->toDateTime()->format('Y-m-d H:i:s'),0,10); //YYYY-MM-DD
 
 
@@ -121,13 +147,13 @@ class DueDate implements DateCalculatorInterface
      * @return bool
      */
     public function isAfterWorkingHour(){
-        $date = Clock::now();
-        $year = substr(Clock::nowAsString('Y-m-d h-m'),0,10); //YYYY-MM-DD
+        $date = Clock::at('2020-09-06 06:00');
+        $year = substr($date->toDateTime()->format('Y-m-d H:i:s'),0,10); //YYYY-MM-DD
 
         if($date->isAfter(Clock::at($year .' 17:00'))){
-            dd("isafter true");
             return true;
         } else {
+            dd("isafter false");
             return false;
         }
     }
@@ -139,7 +165,7 @@ class DueDate implements DateCalculatorInterface
      * @return bool
      */
     public function isInWorkingHour(){
-        $date = Clock::now();
+        $date = Clock::at('2020-09-06 06:00');
         $year = substr(Clock::nowAsString('Y-m-d h-m'),0,10); //YYYY-MM-DD
 
         if($date->isAfter(Clock::at($year .' 09:00')) && $date->isBefore(Clock::at($year .' 17:00'))){
